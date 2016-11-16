00f7706d4ea237df2b9d29c60f00cfe2e05d9a02
Igor Vinokur <igorvin2@gmail.com>
2016-11-16 11:28:29 +0200

    16.11.2016-Update

--- Receiver/Receiver/__vm/.Receiver.vsarduino.h
+++ Receiver/Receiver/__vm/.Receiver.vsarduino.h
@@ -51,5 +51,5 @@ extern "C" void __cxa_pure_virtual() {;}
 #define F(string_literal) ((const PROGMEM char *)(string_literal))
 #undef PSTR
 #define PSTR(string_literal) ((const PROGMEM char *)(string_literal))")
-#include "Transmitter.ino"
+#include "Receiver.ino"
 #endif
