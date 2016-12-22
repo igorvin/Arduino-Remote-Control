d2edaef73b4ebf1f446b0a0865bd325759c2696c
Igor Vinokur <igorvin2@gmail.com>
2016-12-22 18:53:45 +0200

    22-12-2016

--- Receiver/Receiver/Receiver.ino
+++ Receiver/Receiver/Receiver.ino
@@ -28,16 +28,16 @@ struct RECEIVE_DATA_STRUCTURE {
 	byte keysStat = 0;
 	byte AnalogStat1 = 0;	//Analog Trimmer value
 	byte AnalogStat2 = 0;	//Analog Trimmer value
-	byte AnalogStat3 = 0;	//Analog Trimmer value
+	//byte AnalogStat3 = 0;	//Analog Trimmer value
 };
 
 struct SEND_DATA_STRUCTURE {
 	unsigned long enc;
-	byte id = 2;
+	byte id = 0;
 	int outStat = 0;
-	int batV = 0;	//Battary Volt
-	int batA = 0;	//Battary Amper
-	int commstat = 0; //communication status
+	int batV = 0; //Battary Volt
+	int batA = 0; //Battary Amper
+	byte commstat = 0;
 
 };
 
@@ -47,26 +47,30 @@ struct SEND_DATA_STRUCTURE {
 
 long commstat_val = 0; //Test communication status
 long commstat_check_time = 10000;	//Test communication Interval
-long delaycomminit = 3000;
+long delaycomminit = 10000;
 long comminit_val = 0;
 
-byte OutputsState;
+byte OutputsState = 0;
 
-RECEIVE_DATA_STRUCTURE RXData;
-SEND_DATA_STRUCTURE TXData;
 
 #define DEBUG 1 //Enable or Disable Debug option
 
-int ledPins [] = {12, 11, 10, 9, 8, 7, 6, 5}; // GPIO for diffiren uses (like relay, switches)
 
 // create servo object to control a servo
 Servo Servo1;
 Servo Servo2;
-Servo Servo3;
+//Servo Servo3;
+
+int ledPins[] = { 12, 11, 10, 9, 8, 7, 6, 5 }; // GPIO for diffiren uses (like relay, switches)
+int ledstat;
+
+RECEIVE_DATA_STRUCTURE RXData;
+SEND_DATA_STRUCTURE TXData;
+
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	
 for (int index = 0; index <= 7; index++) { pinMode(ledPins[index], OUTPUT);} // Define GPIO to OUTPUT mode
 	
 	//Set Servo pin
@@ -81,16 +85,23 @@ for (int index = 0; index <= 7; index++) { pinMode(ledPins[index], OUTPUT);} // 
 	
 	ETin.begin(details(RXData), &RCSerial);
 	ETout.begin(details(TXData), &RCSerial);
-	TXData.commstat = CommInit;
+	//TXData.commstat = CommInit;
 
-//EEPROM.put(0, 4233); // Atantion!!!! Only if need reset ID
+EEPROM.get(5, RXData.keysStat); // Atantion!!!! Only if need reset ID
+
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
+	if (OutputsState != RXData.keysStat) {
+		
+		SetGPIO();
 
-	
-	if (ETin.receiveData()) {                      // если пришел пакет   
+	}
+
+	//Serial.print("OutputsState: "); Serial.println(OutputsState);
+	//Serial.print("RXData.keysStat: "); Serial.println(RXData.keysStat);
+	if (ETin.receiveData())  {                      // если пришел пакет   
 		Serial.println(RXData.id);
 		if (RXData.id == 1) { // и совпал id
            	EEPROM.get(0, oldCount);               // достаем из EEPROM счетчик
@@ -102,90 +113,117 @@ void loop() {
 			if (count >= oldCount) {                // если счетчик больше сохраненного   
 				count--;                             // отнимаем 1
 				EEPROM.put(0, count);                // пишим в еепром
-				Serial.print("count:"); Serial.println(count);
+			//	Serial.print("count:"); Serial.println(count);
 				Serial.println("Data Recived is correct!!!!");
-				TXData.commstat = 52;
+				TXData.commstat = CommOK;
 
 				if ((millis() - commstat_val) > commstat_check_time) {
 					TXData.commstat = CommOK;
 					commstat_val = millis();
 					Serial.println(TXData.commstat);
-					}
+				}
+
+				SetGPIO();
+				SendData();    //Send Data to Remote unit
+			//	GetGPIOsat();
 
 			
+			}
 
-		for (byte n=0; n<=7; n++)
+}
+}
+	//else 
+	//	Serial.println("ALARM!!! Received Wrong Data");            // received previus packet
+	//if ((millis() - comminit_val) > delaycomminit) {
+	//	Serial.print("ID:"); Serial.println(TXData.id);
+	//	Serial.print("CommStat ID:"); Serial.println(TXData.commstat);
+	//	if (TXData.commstat == CommInit)
+	//		for (int n = 0; n <= 7; n++)
+	//		{
+	//			OutputsState & (1 << digitalRead(ledPins[n]));
+	//		}
+	//		TXData.outStat = OutputsState;
+	//		Serial.println("Send Init data to transmitter");
+		//SendData();
+		//comminit_val = millis();
+	//}
+	}
+
+
+	void SetGPIO() {
+		
+		for (byte n = 0; n <= 7; n++)
 		{
 			//Serial.println(n);
-			
+
 			switch (n) {
 			case 0:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key1 detected");
-				#endif
-				if (RXData.keysStat & (1 << n)) 
+#endif
+				if (RXData.keysStat & (1 << n))
 					digitalWrite(12, HIGH);
 				else
 					digitalWrite(12, LOW);
 				break;
 			case 1:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key2 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(11, HIGH);
 				else
 					digitalWrite(11, LOW);
 				break;
 			case 2:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key3 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(10, HIGH);
 				else
 					digitalWrite(10, LOW);
 				break;
 			case 3:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key4 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(9, HIGH);
 				else
 					digitalWrite(9, LOW);
 				break;
 			case 4:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key5 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(8, HIGH);
 				else
 					digitalWrite(8, LOW);
 				break;
 			case 5:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key6 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(7, HIGH);
 				else
 					digitalWrite(7, LOW);
 				break;
 			case 6:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key7 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(6, HIGH);
 				else
 					digitalWrite(6, LOW);
 				break;
 			case 7:
-				#ifdef DEBUG
+#ifdef DEBUG
 				Serial.println("Key8 detected");
-				#endif
+#endif
 				if (RXData.keysStat & (1 << n))
 					digitalWrite(5, HIGH);
 				else
@@ -193,45 +231,38 @@ void loop() {
 				break;
 			}
 		}
+		OutputsState = RXData.keysStat;
+		EEPROM.put(5, RXData.keysStat);
+	}
 
-		for (int n = 0; n <= 7; n++)
+	void GetGPIOsat(){
+		for (byte n1 = 0; n1 <= 7; n1++)
 		{
-			OutputsState & (1 << digitalRead(ledPins[n]));
-		}
-		//Serial.print("OutputsState:"); Serial.println(OutputsState);
-		TXData.outStat = OutputsState;
-		//Serial.println(TXData.outStat,BIN);
-		delay(1000);
-	
+			if (digitalRead(ledPins[n1]) & (1 << n1));
 
-		SendData(); //Send Data to Remote unit
-	}
+			TXData.outStat ^= 1 << n1;
+			//ledstat = digitalRead(ledPins[n1]);
 
-}
-}
-	else 
-	//	Serial.println("ALARM!!! Received Wrong Data");            // received previus packet
-	if ((millis() - comminit_val) > delaycomminit) {
-		//Serial.print("ID:"); Serial.println(TXData.id);
-		// Serial.print("CommStat ID:"); Serial.println(TXData.commstat);
-		if (TXData.commstat == CommInit)
-			for (int n = 0; n <= 7; n++)
-			{
-				OutputsState & (1 << digitalRead(ledPins[n]));
-			}
-			TXData.outStat = OutputsState;
-			Serial.println("Send Init data to transmitter");
-		SendData();
-		comminit_val = millis();
-	}
+			//Serial.print("Led Pin High: "); Serial.println(ledstat);
+			//Serial.print("OutputsState:"); Serial.println(OutputsState);
+			OutputsState = TXData.outStat;
+			Serial.print("TXData: "); Serial.println(TXData.outStat, BIN);
+			EEPROM.put(5, TXData.outStat);
+
 		}
 
-void SendData() {
-	//Serial.println(MyData.type);
-	EEPROM.get(1, countTX);                          // Get from EEPROM int 
-	countTX--;                                       // take away 1
-	RXData.enc = k.encrypt(countTX);                   // Encrypting data 
-	ETout.sendData();                                 // Send Data 
-	EEPROM.put(1, countTX);                          // Save int to EEPROM
+	}
+
+	void SendData() {
+
+		for (byte n1 = 0; n1 <= 5; n1++){
+			Serial.print("Send ID:   "); Serial.println(TXData.id);
+			EEPROM.get(2, countTX);                          // Get from EEPROM int 
+			countTX--;                                       // take away 1
+			RXData.enc = k.encrypt(countTX);                   // Encrypting data 
+			ETout.sendData();                                 // Send Data 
+			EEPROM.put(2, countTX);                          // Save int to EEPROM
+				delay(200);
 }
+	}
 
--- Receiver/Receiver/__vm/.Receiver.vsarduino.h
+++ Receiver/Receiver/__vm/.Receiver.vsarduino.h
@@ -12,9 +12,8 @@
 #define _VSARDUINO_H_
 #define __AVR_ATmega328p__
 #define __AVR_ATmega328P__
-#define _VMDEBUG 1
 #define F_CPU 16000000L
-#define ARDUINO 10606
+#define ARDUINO 106012
 #define ARDUINO_AVR_NANO
 #define ARDUINO_ARCH_AVR
 #define __cplusplus 201103L
