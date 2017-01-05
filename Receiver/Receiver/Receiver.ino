accef33bed5f46c4059949f5f55b9d9c6f3d215c
Igor Vinokur <igorvin2@gmail.com>
2017-01-05 18:54:57 +0200

    Jumbo Update Release candidat

--- Receiver/Receiver/Receiver.ino
+++ Receiver/Receiver/Receiver.ino
@@ -10,16 +10,16 @@
 #include <EEPROM.h>
 #include <Servo.h>
 #include <SoftwareSerial.h>
-#include <Keeloq.h>
+//#include <Keeloq.h>
 #include <EasyTransfer.h>
 
 //Define Serial port pins
 SoftwareSerial RCSerial(2, 3); // RX, TX
 
-Keeloq k(0x01325324, 0x05064718);        /// Encryption Key
-unsigned int oldCount = 65535;
-unsigned int count; 
-unsigned int countTX = 75535; //ID sent to transmitter
+//Keeloq k(0x01325324, 0x05064718);        /// Encryption Key
+//unsigned int oldCount = 65535;
+//unsigned int count; 
+//unsigned int countTX = 75535; //ID sent to transmitter
 
 EasyTransfer ETin, ETout;
 struct RECEIVE_DATA_STRUCTURE {
@@ -32,7 +32,7 @@ struct RECEIVE_DATA_STRUCTURE {
 };
 struct SEND_DATA_STRUCTURE {
 	unsigned long enc;
-	byte id = 0;
+	int id = 578;
 	int outStat = 0;
 	int batV = 0; //Battary Volt
 	int batA = 0; //Battary Amper
@@ -46,9 +46,11 @@ SEND_DATA_STRUCTURE TXData;
 #define CommOK 55 //Communication OK code
 
 long commstat_val = 0; //Test communication status
-long commstat_check_time = 10000;	//Test communication Interval
+long commstat_check_time = 8000;	//Test communication Interval
 long delaycomminit = 10000;
 long comminit_val = 0;
+long oldAnalogstat1 = 0;
+long oldAnalogstat2 = 0;
 
 byte OutputsState = 0;
 
@@ -59,16 +61,17 @@ Servo Servo1;
 Servo Servo2;
 //Servo Servo3;
 
-int ledPins[] = { 12, 11, 10, 9, 8, 7, 6, 5 }; // GPIO for diffiren uses (like relay, switches)
+int gpioPins[] = { 12, 11, 10, 9, 8, 7, 6, 5 }; // GPIO for diffiren uses (like relay, switches)
 int ledstat;
 // the setup function runs once when you press reset or power the board
 void setup() {
 
 // Define GPIO to OUTPUT mode	
-	for (int index = 0; index <= 7; index++) { pinMode(ledPins[index], OUTPUT);} 
+	for (int index = 0; index <= 7; index++) { pinMode(gpioPins[index], OUTPUT);} 
+
 //Set Servo pins
-	Servo1.attach(13);
-	Servo2.attach(4);
+	Servo1.attach(4);
+	Servo2.attach(13);
 	Servo1.write(0);
 	Servo2.write(0);
 	//Servo3.attach(4);
@@ -90,25 +93,20 @@ EEPROM.get(5, RXData.keysStat);
 // the loop function runs over and over again until power down or reset
 void loop() {
 	if (OutputsState != RXData.keysStat) {
-		
 		SetGPIO();
-
-	}	
+	}
+	//Send Coomunication status every 10 sec
+	if ((millis() - commstat_val) > commstat_check_time) {
+		TXData.commstat = CommOK;
+		commstat_val = millis();
+		Serial.println(TXData.commstat);
+		SendData();
+	}
 	if (ETin.receiveData())  {                      // если пришел пакет   
 		Serial.println(RXData.id);
 		if (RXData.id == 1) { // и совпал id
-           	EEPROM.get(0, oldCount);               // достаем из EEPROM счетчик
-			count = k.decrypt(RXData.enc);           // декодируем 
-#ifdef DEBUG
-			Serial.print("oldcount:"); Serial.println(oldCount);
-			
-#endif // !DEBUG
-			if (count >= oldCount) {                // если счетчик больше сохраненного   
-				count--;                             // отнимаем 1
-				EEPROM.put(0, count);                //write to eeprom
-			//	Serial.print("count:"); Serial.println(count);
-				Serial.println("Data Recived is correct!!!!");
-				TXData.commstat = CommOK;
+          		TXData.commstat = CommOK;
+		}
 //Send Coomunication status every 10 sec
 				if ((millis() - commstat_val) > commstat_check_time) {
 					TXData.commstat = CommOK;
@@ -117,26 +115,13 @@ void loop() {
 				}
 
 				SetGPIO();//Set GPIO function
-				SendData();    //Send Data to Remote unit 
-			//	GetGPIOsat();
+				
+			GetGPIOsat();
+			SendData();    //Send Data to Remote unit 
+			
 			}
-			}
-}
-	//else 
-	//	Serial.println("ALARM!!! Received Wrong Data");            // received previus packet
-	//if ((millis() - comminit_val) > delaycomminit) {
-	//	Serial.print("ID:"); Serial.println(TXData.id);
-	//	Serial.print("CommStat ID:"); Serial.println(TXData.commstat);
-	//	if (TXData.commstat == CommInit)
-	//		for (int n = 0; n <= 7; n++)
-	//		{
-	//			OutputsState & (1 << digitalRead(ledPins[n]));
-	//		}
-	//		TXData.outStat = OutputsState;
-	//		Serial.println("Send Init data to transmitter");
-		//SendData();
-		//comminit_val = millis();
-	//}
+
+	
 	}
 
 	void SetGPIO() {
@@ -220,16 +205,26 @@ void loop() {
 		}
 		OutputsState = RXData.keysStat;
 		EEPROM.put(5, RXData.keysStat);
+		if ((oldAnalogstat1 != RXData.AnalogStat1) || (oldAnalogstat2 != RXData.AnalogStat2)) {
+			Serial.print("AnalogStat1:   "); Serial.println(RXData.AnalogStat1);
+			Serial.print("AnalogStat2:   "); Serial.println(RXData.AnalogStat2);
+			Servo1.write(RXData.AnalogStat1);
+			Servo2.write(RXData.AnalogStat2);
+			oldAnalogstat1 == RXData.AnalogStat1;
+			oldAnalogstat2 == RXData.AnalogStat2;
+				}
+
 	}
 	void GetGPIOsat(){
 		for (byte n1 = 0; n1 <= 7; n1++)
 		{
-			if (digitalRead(ledPins[n1]) & (1 << n1));
+			if (digitalRead(gpioPins[n1]) == 1)
 
-			TXData.outStat ^= 1 << n1;
-			//ledstat = digitalRead(ledPins[n1]);
-
-			//Serial.print("Led Pin High: "); Serial.println(ledstat);
+				TXData.outStat |= 1 << n1;
+			else
+				TXData.outStat &= ~(1 << n1);
+		}
+			//Serial.print("Led Pin : "); Serial.println(ledstat);
 			//Serial.print("OutputsState:"); Serial.println(OutputsState);
 			OutputsState = TXData.outStat;
 			Serial.print("TXData: "); Serial.println(TXData.outStat, BIN);
@@ -237,17 +232,12 @@ void loop() {
 
 		}
 
-	}
+	
 	void SendData() {
 
-		for (byte n1 = 0; n1 <= 5; n1++){
+		//for (byte n1 = 0; n1 <= 1; n1++){
 			Serial.print("Send ID:   "); Serial.println(TXData.id);
-			EEPROM.get(2, countTX);                          // Get from EEPROM int 
-			countTX--;                                       // take away 1
-			RXData.enc = k.encrypt(countTX);                   // Encrypting data 
 			ETout.sendData();                                 // Send Data 
-			EEPROM.put(2, countTX);                          // Save int to EEPROM
 				delay(200);
-}
 	}
 
